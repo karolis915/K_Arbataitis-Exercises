3b3e7e592ffe1fa2686ae4833f0c0a0589d72f7a
karolis915 <151562263+karolis915@users.noreply.github.com>
2023-12-28 19:11:58 +0200

    Delete PD4 directory

--- PD4/PD4_6.java
+++
@@ -1,9 +0,0 @@
-public class PD4_6 {
-    public static void main(String[] args) {
-        int intNum1 = 10;
-        int intNum2 = 50;
-
-        int rez = (intNum1 <= intNum2) ? (intNum1 * intNum2) : (intNum1 + intNum2);
-        System.out.println(rez);
-    }
-}
--- PD4/PD4_7.java
+++
@@ -1,7 +0,0 @@
-public class PD4_7 {
-    public static void main(String[] args) {
-      float myFloat = 1 + 5 % 3 * 1 - 2;
-
-       System.out.println(myFloat);
-    }
-}
