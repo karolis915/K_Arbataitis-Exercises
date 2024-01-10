4dd592522c6fdcb150baa394d0db891675c5aece
karolis915 <151562263+karolis915@users.noreply.github.com>
2024-01-10 11:18:23 +0200

    Delete K_Arbataitis-Exercises/PD6 directory

--- K_Arbataitis-Exercises/PD6/PD6_2.java
+++
@@ -1,9 +0,0 @@
-public class PD6_2 {
-
-    public static void main(String[] args){
-        int [] array = {3, 9, 13, 30};
-        System.out.println(array[0]);
-        System.out.println(array[3]);
-    }
-
-}
